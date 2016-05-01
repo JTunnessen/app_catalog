a44e0c79ed496b7e46de29a12374a8fc7b8d5add
@JimT2 <jtunnessen@gmail.com>
2016-05-01 07:48:04 -0400

    Added UI to the User login page and registrations, put in before_filter for users

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,5 @@
 class AppsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_app, only: [:show, :edit, :update, :destroy]
 
   # GET /apps
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,2 +1,5 @@
 class App < ActiveRecord::Base
+
+  
+
 end
