45f22f04f91d7c5fdaa009ea1b759b6b017169cf
@JimT2 <jtunnessen@gmail.com>
2016-05-01 13:04:18 -0400

    Added panels to the app views and prioritization logic to app cont

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,7 +5,7 @@ class AppsController < ApplicationController
   # GET /apps
   # GET /apps.json
   def index
-    @apps = App.all
+    @apps = App.order("apps.app_priority ASC").all
   end
 
   # GET /apps/1
