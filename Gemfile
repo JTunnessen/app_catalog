17067ebf9241f2288985a476e72bf009e312ba75
@JimT2 <jtunnessen@gmail.com>
2016-05-14 23:15:52 -0400

    Added charts to dashboard

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# Grouping of dates
+gem 'groupdate'
 # Graphs and Charts
 gem "chartkick"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   end
 
 
+  #$app_user_count = App.fetch(:user_count_1).to_i
+
+# Dashboard Equations -- EQUATION IS WRONG
+  total_user_count1 = App.sum(:user_count_1).delete(',').to_i + App.sum(:user_count_2).delete(',').to_i + App.sum(:user_count_3).delete(',').to_i + App.sum(:user_count_4).delete(',').to_i
+  total_user_count2 = App.sum(:user_count_5).delete(',').to_i + App.sum(:user_count_6).delete(',').to_i + App.sum(:user_count_7).delete(',').to_i + App.sum(:user_count_8).delete(',').to_i
+  $total_user_count = total_user_count1 + total_user_count2
+
 end
