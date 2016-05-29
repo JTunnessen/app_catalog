983458cdc97b6a5438dd2af837e896c772615f1e
jimt2 <jtunnessen@gmail.com>
2016-05-29 14:52:09 -0400

    Clean up after field name changes

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -9,9 +9,9 @@ module AppsHelper
  #tech_eol_1 = (app.tech_stack_eol_1 - Date.today)
  #tech_eol_2 = (app.tech_stack_eol_2 - Date.today)
  #tech_eol_3 = (app.tech_stack_eol_3 - Date.today)
- #tech_eol_4 = (app.tech_stack_eol_4 - Date.today)
- #tech_eol_5 = (app.tech_stack_eol_5 - Date.today)
- #tech_eol_6 = (app.tech_stack_eol_6 - Date.today)
+ #tech_eol_4 = (app.app_server_eol - Date.today)
+ #tech_eol_5 = (app.web_server_eol - Date.today)
+ #tech_eol_6 = (app.database_eol - Date.today)
 
  #<% if (prog_eol < year1) || (framework_eol < year1) || (tech_eol_1 < year1) || (tech_eol_2 < year1) || (tech_eol_3 < year1) || (tech_eol_4 < 1Year) %>
    #<% (tech_eol_5 < year1) || (tech_eol_6 < year1) %>
